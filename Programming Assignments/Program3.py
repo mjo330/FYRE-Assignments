38999a2a77a0363d74ac03278297950702dc74e2
Micah Oliveri-Schneider <mjo330@lehigh.edu>
2026-09-09 15:21:25 -0400

    Add files via upload

---
+++ Programming Assignments/Program3.py
@@ -0,0 +1,4 @@
+# This program was created in Arduino Lab for MicroPython
+# variable to store my name
+name = "Micah Oliveri-Schneider"
+print(name) # output the contents of the variable "name" to the REPL
